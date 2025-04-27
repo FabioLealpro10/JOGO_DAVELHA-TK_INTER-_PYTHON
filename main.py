660f5b4c3b5bb879f43d487971ef7667c61e60d2
Fabio Leal <fl2646730@gmail.com>
2025-04-26 21:28:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from classe import jogo
 
 
 def rodar():
-    # opicional: passa como paramentro o nome do jogador1 e no outro o jogador 2
-
-
     JOGO = jogo() # ex: jogo('j1', 'j2')
     JOGO.execute()
 
